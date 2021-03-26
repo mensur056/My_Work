dfeb34ad010fe440234a26ed20d04e7cb9b10693
Mansur Sarkhanov <mansur.sarxanov@gmail.com>
2021-03-26 10:35:34 +0200

    today

---
+++ day1+.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Person{
+public:
+string Name,Surname,City,Work;
+int Age;
+void IntroduceYourself(){
+    cout<<"Name -"<<Name<<endl;
+    cout<<"Surname -"<<Surname<<endl;
+    cout<<"City -"<<City<<endl;
+    cout<<"Work -"<<Work<<endl;
+    cout<<"Age -"<<Age<<endl;
+
+}Person(string name,string surname,string city,string work,int age){
+    Name=name;
+    Surname=surname;
+    City=city;
+    Work=work;
+    Age=age;}
+};
+int main(){
+   
+    Person person1=Person("Mansur","Sarkhanov","Azerbaijan","Engineer",23);
+    person1.IntroduceYourself();
+    Person person2=Person("Emil","Qeniyev","Germany","Lowyer",20);
+    person2.IntroduceYourself();
+}
+
+
+
--- day1.cpp
+++ day1.cpp
@@ -1,1 +1,13 @@
 #include<iostream>
+using namespace std;
+int main(){
+int a,b,c;
+cout<<"enter number a="<<endl;
+cin>>a;
+cout<<"enter number b="<<endl;
+cin>>b;
+cout<<"a+b="<<a+b<<endl;
+cout<<"a-b="<<a-b<<endl;
+cout<<"a*b="<<a*b<<endl;
+cout<<"a/b="<<a/b<<endl;
+}
