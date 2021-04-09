4627a33c02632a8a51c16300922dc58576e70a9d
mensur056 <mansur.sarxanov@gmail.com>
2021-04-09 08:30:10 +0400

    Test

---
+++ day3+.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+    cout<<"you enter a number =";
+    cin>>x
+    if(x>5){
+
+        cout<<"daxil ettiyiniz eded 5 den boyukdur";
+    }else {
+        cout<<"daxil ettiyiniz eded 5 den kicikdir";
+
+    }
+}
