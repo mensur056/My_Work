4d8f1637e23df126da5a1f614b66c1a0ee4b9b19
mensur056 <mansur.sarxanov@gmail.com>
2021-04-12 16:46:22 +0400

    TEST

--- day3+.cpp
+++ day3+.cpp
@@ -3,12 +3,11 @@ using namespace std;
 int main(){
 int x;
     cout<<"you enter a number =";
-    cin>>x
+    cin>>x;
+
     if(x>5){
-
-        cout<<"daxil ettiyiniz eded 5 den boyukdur";
-    }else {
-        cout<<"daxil ettiyiniz eded 5 den kicikdir";
-
+ cout<<"daxil ettiyiniz eded 5 den boyukdur";
     }
+
+    
 }
