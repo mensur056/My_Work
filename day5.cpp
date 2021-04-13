a3750b1ebc39e17e0979c91e8de7aab9999eaa61
mensur056 <mansur.sarxanov@gmail.com>
2021-04-13 14:00:57 +0400

    tests

---
+++ day5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int y,x;
+cout<<"you enter x=";
+cin>>x>>endl;
+cout<<"you enter y=";
+cin>>y;
+if(x>y){
+    cout<<"number2 than big number1";
+}else{
+    cout<<"number2 than small number1";
+}
+
+
+}
